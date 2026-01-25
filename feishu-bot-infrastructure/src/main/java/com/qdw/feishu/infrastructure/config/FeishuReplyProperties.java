e71c998e4356dcd590f71b492f65e61df1fa4674
qdw497874677 <5508693+quan_da_wei@user.noreply.gitee.com>
2026-01-25 22:17:44 +0800

    docs: 添加回复模式配置类文档注释

--- feishu-bot-infrastructure/src/main/java/com/qdw/feishu/infrastructure/config/FeishuReplyProperties.java
+++ feishu-bot-infrastructure/src/main/java/com/qdw/feishu/infrastructure/config/FeishuReplyProperties.java
@@ -5,10 +5,16 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+/**
+ * 飞书回复配置属性
+ */
 @Data
 @Component
 @ConfigurationProperties(prefix = "feishu.reply")
 public class FeishuReplyProperties {
 
+    /**
+     * 回复模式
+     */
     private ReplyMode mode = ReplyMode.DEFAULT;
 }
